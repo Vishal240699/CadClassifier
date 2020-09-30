cf0008a0f428b50d5c4a8b344dabaae88c970fed
vishal240699 <vishalkumar_ma@srmuniv.edu.in>
2020-09-30 12:22:37 +0530

    comments

--- image_augmentation.py
+++ image_augmentation.py
@@ -1,21 +1,21 @@
+import numpy as np
+import random
+import os
 import matplotlib
 matplotlib.use("TkAgg")
 from keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot as plt
-import numpy as np
-import random
-import os
 
 src = 'Dataset/PetImages/'
 
-# Check if the dataset has been downloaded. If not, direct user to download the dataset first
+# Checks if the dataset has been downloaded.
 if not os.path.isdir(src):
     print("""
           Dataset not found in your computer.
-          Please follow the instructions in the link below to download the dataset:
-          https://raw.githubusercontent.com/PacktPublishing/Neural-Network-Projects-with-Python/master/chapter4/how_to_download_the_dataset.txt
+          Please download.
           """)
     quit()
+
 
 image_generator = ImageDataGenerator(rotation_range = 30,
                                      width_shift_range = 0.2,
@@ -27,14 +27,17 @@ image_generator = ImageDataGenerator(rotation_range = 30,
 fig, ax = plt.subplots(2,3, figsize=(20,10))
 all_images = []
 
+
 _, _, dog_images = next(os.walk('Dataset/PetImages/Train/Dog/'))
 random_img = random.sample(dog_images, 1)[0]
 print(random_img)
 random_img = plt.imread('Dataset/PetImages/Train/Dog/'+random_img)
 all_images.append(random_img)
 
+
 random_img = random_img.reshape((1,) + random_img.shape)
 sample_augmented_images = image_generator.flow(random_img)
+
 
 for _ in range(5):
 	augmented_imgs = sample_augmented_images.next()
